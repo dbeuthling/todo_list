9bcb59d6d4233218eba00b397035ece6141b625a
Dan Beuthling <danbeuthling@gmail.com>
2016-06-02 15:15:48 -0500

    refactored validations

--- todo.rb
+++ todo.rb
@@ -26,15 +26,32 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Return an error message if invalid. Otherwise nil.
+def error_for_list_name(name)
+  if session[:lists].any? { |list| list[:name] == name }
+    "The list name must be unique."
+  elsif !(1..100).cover? name.length
+    "The list name must be between 1 and 100 characters."
+  end
+end
+
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
-  if (1..100).cover? list_name.length
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
     session[:lists] << { name: list_name, todos: [] }
     session[:success] = "This list has been added successfully"
     redirect "/lists"
-  else
-    session[:error] = "The list name must be between 1 and 100 characters long."
-    redirect "/lists/new"
   end
+end
+
+get "/lists/:id" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :list, layout: :layout
 end
